c0a332b0ebd2f90ed9efc9ab13e3ca1f7c438950
Lloyd DSilva <lloyddsilva@gmail.com>
2013-10-26 15:35:39 +0800

    Base social features

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialFeatures::Application.config.secret_token = '4290d187003449f7b92c7fe45910a362d6a3eb6c9db855ec83d61ce561abf84c6fa8742c1b2b399504b07b3b5d2bae879a0062261f0e3ac2c8692ece64231129'
